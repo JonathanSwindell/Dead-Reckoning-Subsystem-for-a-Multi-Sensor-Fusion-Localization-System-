ddb877d1424a85d5cd8cd7d2efdfd0c3e5d90da5
agm0025 <55323906+agm0025@users.noreply.github.com>
2023-02-10 16:43:39 -0600

    IMU Rotation. WIP.
    First steps to try to figure out how the rotation data works, and how to apply that to other data from the BNO055 to adjust from the sensor's frame to the global NED frame of reference.

---
+++ .ipynb_checkpoints/Rotation.py
@@ -0,0 +1,57 @@
+from functools import partial
+
+import numpy as np
+from pyquaternion import Quaternion
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import FuncAnimation
+
+def main():
+    arr = np.loadtxt("rotdata.csv",
+                     delimiter=",", dtype=str)
+    fig = plt.figure()
+
+    # Defining the axes as a
+    # 3D axes so that we can plot 3D
+    # data into it.
+    ax = plt.axes(projection="3d")
+    anim = FuncAnimation(fig, animate, fargs=(ax, arr), frames=10, interval=20, repeat=True)
+    #animate(0, ax, arr)
+    plt.show()
+
+
+def animate(i, ax, arr):
+    ax.clear()
+    ax.set_xlim([-10, 10])
+    ax.set_ylim([-10, 10])
+    ax.set_zlim([-10, 10])
+    xi = int(arr[i][0])/100
+    x = [0, xi] # Add a zero to the point so that matplotlib will draw it properly.
+    yi=int(arr[i][1])/100
+    y = [0, yi]
+    zi = int(arr[i][2])/100
+    z = [0, zi]
+
+    quat_array = arr[i,3:7].astype(float)
+    quat_array = quat_array / (16384-1) # Quaternion data ranges from 0-2^14-1.
+    q = Quaternion(quat_array)
+
+    vect = [0, xi, yi, zi] # Convert vector to quaternion
+
+    accq = Quaternion(vect)
+
+    result = q.conjugate*accq*q # This should apply the opposite rotation to acceleration
+
+    final_vector = result.elements
+
+    x2 = [0, final_vector[0]]
+    y2 = [0, final_vector[1]]
+    z2 = [0, final_vector[2]]
+
+    ax.plot3D(x, y, z, 'red')
+    ax.plot3D(x2, y2, z2, 'green')
+
+
+
+if (__name__ == "__main__"):
+    main()
--- InertialMeasurementInterface/InertialMeasurementInterface.ino
+++ InertialMeasurementInterface/InertialMeasurementInterface.ino
@@ -1,45 +1,58 @@
-#include "BNO055_support.h"   //Contains the bridge code between the API and Arduino
+o0#include "BNO055_support.h"   //Contains the bridge code between the API and Arduino
 #include <Wire.h>
 
 struct bno055_t myBNO;
 typedef struct bno055_Inertial_data_t{ 
     bno055_accel AccelData;
     bno055_mag   MagData;
-    bno055_gyro  GyroData;
+    bno055_gyro  GyroData; 
+    bno055_euler EulerData; // Divide by 16 to get values in degrees
+    bno055_quaternion QuatData;
+    
  } bno055_Inertial_data_t; // Raw Real and Imaginary values
 
 bno055_Inertial_data_t bno055_Inertial_data;
 
+void convertRotation(bno055_Inertial_data_t*, double*);
+
 unsigned long lastTime = 0;
 
+// This function is used to generate a csv of the data for debugging.
+void rotDebug(bno055_Inertial_data_t bno055_Inertial_data) {
+  Serial.print(bno055_Inertial_data.AccelData.x);
+  Serial.print(",");
+  Serial.print(bno055_Inertial_data.AccelData.y);
+  Serial.print(",");
+  Serial.print(bno055_Inertial_data.AccelData.z);
+  Serial.print(",");
+  Serial.print(float(bno055_Inertial_data.QuatData.w));
+  Serial.print(",");
+  Serial.print(float(bno055_Inertial_data.QuatData.x));
+  Serial.print(",");
+  Serial.print(float(bno055_Inertial_data.QuatData.y));
+  Serial.print(",");
+  Serial.print(float(bno055_Inertial_data.QuatData.z));
+  Serial.println(",");
+}
 void SerialPrintInertialData(bno055_Inertial_data_t bno055_Inertial_data){
     Serial.print("Time Stamp: ");       //To read out the Time Stamp
     Serial.println(lastTime);
+    
+    Serial.print("Acceleration (x): ");        
+    Serial.print(float(bno055_Inertial_data.AccelData.x)/100); // Divide to account to convert to float. Based on g = 9.8 m/s^2       
+    Serial.print(" (y): ");       
+    Serial.print(float(bno055_Inertial_data.AccelData.y)/100);        
+    Serial.print(" (z): ");       
+    Serial.println(float(bno055_Inertial_data.AccelData.z)/100);       
 
-    Serial.print("Acceleration (x): ");  
-    //Serial.print(",");        
-    Serial.println(float(bno055_Inertial_data.AccelData.x)/100); // Divide to account to convert to float. Based on g = 9.8 m/s^2   
-    //Serial.print(",");       
-
-    Serial.print("Acceleration (y): ");       
-    Serial.println(float(bno055_Inertial_data.AccelData.y)/100);   
-    //Serial.print(",");       
-
-
-    Serial.print("Acceleration (z): ");       
-    Serial.println(float(bno055_Inertial_data.AccelData.z)/100);   
-    //Serial.print(",");          
-
-
+    /*
     Serial.print("Magnetometer (x): ");       
-    Serial.println(float(bno055_Inertial_data.MagData.x));   
-
-    Serial.print("Magnetometer (y): ");       
-    Serial.println(float(bno055_Inertial_data.MagData.y));   
-
-    Serial.print("Magnetometer (z): ");       
+    Serial.print(float(bno055_Inertial_data.MagData.x));   
+    Serial.print(" (y): ");       
+    Serial.print(float(bno055_Inertial_data.MagData.y));   
+    Serial.print(" (z): ");       
     Serial.println(float(bno055_Inertial_data.MagData.z));   
-
+    /*
     Serial.print("Gyroscope (x): ");       
     Serial.println(float(bno055_Inertial_data.GyroData.x));   
 
@@ -48,6 +61,34 @@ void SerialPrintInertialData(bno055_Inertial_data_t bno055_Inertial_data){
 
     Serial.print("Gyroscope (z): ");       
     Serial.println(float(bno055_Inertial_data.GyroData.z));   
+  */
+    Serial.print("Euler  Angles: Heading: ");
+    Serial.print(float(bno055_Inertial_data.EulerData.h) / 16.0);
+    Serial.print(" Roll: ");
+    Serial.print(float(bno055_Inertial_data.EulerData.r)  / 16.0);
+    Serial.print(" Pitch: ");
+    Serial.println(float(bno055_Inertial_data.EulerData.p)  / 16.0);
+
+    Serial.print("Quaternion: w: ");
+    Serial.print(float(bno055_Inertial_data.QuatData.w));
+    Serial.print(" x: ");  
+    Serial.print(float(bno055_Inertial_data.QuatData.x));
+    Serial.print(" y: ");  
+    Serial.print(float(bno055_Inertial_data.QuatData.y));
+    Serial.print(" z: ");  
+    Serial.println(float(bno055_Inertial_data.QuatData.z));
+
+    /*
+    double newAccel[3] = {0.0, 0.0, 0.0};
+    convertRotation(&bno055_Inertial_data, newAccel);
+    Serial.print("Constant Accel: x: ");
+    Serial.print(newAccel[0]);
+    Serial.print(" y: ");
+    Serial.print(newAccel[1]);
+    Serial.print(" z: ");
+    Serial.println(newAccel[2]);
+    */
+    
 
 }
 
@@ -55,6 +96,37 @@ void getInertialMeasurements(bno055_Inertial_data_t * bno055_Inertial_data){
     bno055_read_accel_xyz(&(bno055_Inertial_data->AccelData));  //Update Acceleration data into the structure
     bno055_read_gyro_xyz(&(bno055_Inertial_data->GyroData));    //Update Gyroscope data into the structure
     bno055_read_mag_xyz(&(bno055_Inertial_data->MagData));      //Update Magnetometer data into the structure
+    bno055_read_euler_hrp(&(bno055_Inertial_data->EulerData));
+    bno055_read_quaternion_wxyz(&(bno055_Inertial_data->QuatData));
+}
+double dotProduct(int n, double* a, double* b) {
+  double sum = 0.0;
+  for (int i = 0; i < n; i++) {
+      sum = sum + (a[i]*b[i]);
+  }  
+  return sum;
+}
+void convertRotation(bno055_Inertial_data_t* bno, double* returnAccel) {
+  // This function DOES NOT work. It attempts to use a rotation matrix to undo the rotation.
+  double heading = ((double)bno->EulerData.h / 16.0) * 0.017453; // Convert to radians
+  double roll = ((double)bno->EulerData.r  / 16.0) * 0.017453;
+  double pitch = ((double)bno->EulerData.p  / 16.0) * 0.017453;
+
+  double ax = (double)bno->AccelData.x / 100.0;
+  double ay = (double)bno->AccelData.y / 100.0;
+  double az = (double)bno->AccelData.z / 100.0;
+
+  double xyz[3] = {ax, ay, az};
+
+  // One issue is that this is I think an incorrect rotation matrix
+  // Another issue is that the inverse needs to be used to undo the rotation
+  double R1[3] = {cos(heading)*cos(pitch), sin(heading)*cos(pitch), -1*sin(pitch)};
+  double R2[3] = {cos(heading)*sin(pitch)*sin(roll)-sin(heading)*cos(roll), sin(heading)*sin(pitch)*sin(roll) + cos(heading)*cos(roll), cos(pitch)*sin(roll)};
+  double R3[3] = {cos(heading)*sin(pitch)*cos(roll) + sin(heading)*sin(roll), sin(heading)*sin(pitch)*cos(roll)-cos(heading)*sin(roll), cos(pitch)*cos(roll)};
+  
+  returnAccel[0] = dotProduct(3, xyz, R1);
+  returnAccel[1] = dotProduct(3, xyz, R2);
+  returnAccel[2] = dotProduct(3, xyz, R3);
 }
 
 void setup() 
@@ -80,7 +152,7 @@ void loop() //This code is looped forever
     lastTime = millis();
 
     getInertialMeasurements(&bno055_Inertial_data);
-   
-    SerialPrintInertialData(bno055_Inertial_data);
+    rotDebug(bno055_Inertial_data);
+    //SerialPrintInertialData(bno055_Inertial_data);
   }
 }
